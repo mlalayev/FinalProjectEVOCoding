e20d31c8809c117aba803b8edaf4ed9a41f3e258
Murad Lalayev <lalayevmurad@gmail.com>
2024-07-03 18:59:31 +0400

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import MainPage from './Components/MainPage/MainPage.jsx'
+import { I18nextProvider } from 'react-i18next';
+import i18n from './Components/I18N/I18N.jsx';
 
 function App() {
   return (
     <>
-      <MainPage />
+      <I18nextProvider i18n={i18n}>
+        <MainPage />
+      </I18nextProvider>
     </>
   )
 }
