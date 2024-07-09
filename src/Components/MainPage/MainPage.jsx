d495ec8058bfd1b34bd924d3c79d345fd02bec48
Murad Lalayev <lalayevmurad@gmail.com>
2024-07-09 21:13:28 +0400

    my commit

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useTranslation } from 'react-i18next';
-import ImageSlider from '../../Components/ImageSlider/ImageSlider.jsx';
 import './MainPage.css'
+import { useTranslation } from 'react-i18next';
+import React, { useState, useEffect, useRef } from 'react';
+import sectionfirstContent from '../../../SectionFirstMap.json';
+import ImageSlider from '../../Components/ImageSlider/ImageSlider.jsx';
 
 function MainPage() {
   const { i18n } = useTranslation();
-
   const [slides, setSlides] = useState([]);
+
 
   useEffect(() => {
     const fetchSlides = async () => {
@@ -23,10 +24,24 @@ function MainPage() {
   }, [i18n.language]);
 
 
+
+
   return (
     <div id='body'>
       <section className="sectionslider">
         {slides.length > 0 ? <ImageSlider slides={slides} /> : <p>Loading...</p>}
+      </section>
+
+      <section className="sectionfirst">
+        {sectionfirstContent.map((item) => (
+          <div className="sectionfirst__item" key={item.id}>
+            <div className="sectionfirst__item__img">
+              <img src={item.img} alt="" className="sectionfirst-image" />
+              <h1 className="sectionfirst-htag">{item.title[i18n.language]}</h1>
+              <p className="sectionfirst-ptag">{item.text[i18n.language]}</p>
+            </div>
+          </div>
+        ))}
       </section>
     </div>
   );
