8893c3f400c67487d9763828478ff2ed609857af
Murad Lalayev <lalayevmurad@gmail.com>
2024-07-10 18:19:14 +0400

    my commit

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -5,8 +5,24 @@ import sectionfirstContent from '../../../SectionFirstMap.json';
 import ImageSlider from '../../Components/ImageSlider/ImageSlider.jsx';
 
 function MainPage() {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const [slides, setSlides] = useState([]);
+
+  const initialTime = 143 * 24 * 60 * 60 + 21 * 60 * 60 + 12 * 60 + 30;
+  const [time, setTime] = useState(initialTime);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(prevTime => (prevTime > 0 ? prevTime - 1 : 0));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const days = Math.floor(time / (24 * 60 * 60));
+  const hours = Math.floor((time % (24 * 60 * 60)) / (60 * 60));
+  const minutes = Math.floor((time % (60 * 60)) / 60);
+  const seconds = Math.floor(time % 60);
+
 
 
   useEffect(() => {
@@ -42,6 +58,42 @@ function MainPage() {
             </div>
           </div>
         ))}
+        <div className="inputholder-sectionfirst">
+          <h1>{t('mainpage.registrationtextone')}</h1>
+          <h1>{t('mainpage.registrationtexttwo')}</h1>
+
+          <div className="inputs">
+            <input type="text" placeholder={t('mainpage.inputtextone')} className="input" />
+            <input type="text" placeholder={t('mainpage.inputtexttwo')} className="input" />
+            <input type="text" placeholder={t('mainpage.inputtextthree')} className="input" />
+            <select name="" id="select">
+              <option value="">C Developer</option>
+              <option value="">Java Developer</option>
+              <option value="">Back-End Developer</option>
+              <option value="">Front-End Developer</option>
+            </select>
+            <button className='inputs-button'>{t('mainpage.buttonfirsttext')}</button>
+          </div>
+
+          <div className="countdowntimer">
+            <div>
+              <p>{days}</p>
+              <p>Days</p>
+            </div>
+            <div>
+              <p>{hours}</p>
+              <p>Hours</p>
+            </div>
+            <div>
+              <p>{minutes}</p>
+              <p>Minutes</p>
+            </div>
+            <div>
+              <p>{seconds}</p>
+              <p>Seconds</p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
