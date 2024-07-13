5d916ab3beb70413b649fc94f16d93afaf4e5cd5
Guseynov62 <familh21@gmail.com>
2024-07-13 19:38:22 +0200

    search

--- src/Courses/course.jsx
+++ src/Courses/course.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import '../Courses/course.css'
 // icons...
 import { MdNavigateNext } from "react-icons/md";
@@ -8,112 +8,108 @@ import { IoSearchSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { GrLinkNext } from "react-icons/gr";
 // icons...
-import Course_cards from "./course_cards";
-// img...
-import Card1 from '../../src/assets/Cards/card1.jpg';
-import Card2 from '../../src/assets/Cards/card2.jpg';
-import Card3 from '../../src/assets/Cards/card3.jpg';
-import Card4 from '../../src/assets/Cards/card4.jpg';
-import Card5 from '../../src/assets/Cards/card5.jpg';
-import Card6 from '../../src/assets/Cards/card6.jpg';
-import Card7 from '../../src/assets/Cards/card7.jpg';
-import Card8 from '../../src/assets/Cards/card8.jpg';
-import View from "./view";
-// img...
-const Course =()=>{
-return(
+import { TbClockHour4Filled } from "react-icons/tb";
+import { FaSignal } from "react-icons/fa";
+import { MdCollectionsBookmark } from "react-icons/md";
+import { FaPuzzlePiece } from "react-icons/fa6";
+import { PiStudentBold } from "react-icons/pi";
+
+import {data} from '../Courses/json'
+
+const Course = () => {
+  const [search,setSearch]=useState('')
+  
+  return (
     <main className="course">
-        <div className="home-course">
-         <Link to={'/'} style={{textDecoration:'none'}}><p style={{color:'#ACACAC',cursor:'pointer'} }>Home </p> </Link>
-         <p style={{marginTop:'7px'}}>{<MdNavigateNext />}</p>
-         <p>All courses</p>
+      <div className="home-course">
+        <Link to={'/'} style={{ textDecoration: 'none' }}><p style={{ color: '#ACACAC', cursor: 'pointer' }}>Home </p> </Link>
+        <p style={{ marginTop: '7px' }}>{<MdNavigateNext />}</p>
+        <p>All courses</p>
+      </div>
+
+      <div className="search">
+        <h1>All Courses</h1>
+        <div className="search-right">
+          <div className="inputSearch">
+            <input type="text" name="text" id="text" placeholder="Seach courses..." style={{ paddingLeft: '20px' }} 
+             onChange={(e)=>setSearch(e.target.value)}
+            />
+            <p>{<IoSearchSharp />}</p>
+          </div>
+          <p className="icon">{<TfiLayoutGrid3Alt />}</p>
+          <p className="icon">{<TfiMenuAlt />}</p>
         </div>
-         
-         <div className="search">
-            <h1>All Courses</h1>
-            <div className="search-right">
-                <div className="inputSearch">
-                  <input type="text" name="text" id="text" placeholder="Seach courses..." style={{paddingLeft:'20px'}} />
-                  <p>{<IoSearchSharp />}</p>
-                </div>
-                <p className="icon">{<TfiLayoutGrid3Alt />}</p>
-                <p className="icon">{<TfiMenuAlt />}</p>
+      </div>
+      
+ {data.filter((item)=>{
+          return search.toLocaleLowerCase() === '' ? item : item.skill.toLowerCase().includes(search) 
+        }).map((item) => (
+      <div className="cards" key={item.id}>
+
+        <div className="photo">
+          <img src={item.image} alt="" />
+        </div>
+
+        <div className="information">
+          <div className="card_up">
+            <div className="button_up">
+              <span>{item.skill}</span>
             </div>
-         </div>
-         
-         <Course_cards 
-          price='$160.00'
-          course_name ='Software Development'
-          skill='Computer Science'
-          name='Adam Smith'
-          img={Card1}
-        />
-        
-        <Course_cards 
-          price='$140.00'
-          course_name ='Business'
-          skill='Business Essentials'
-          name='Jarek Majiek'
-          img={Card2}
-        />
+            <a href="#" style={{ color: '#72ACFF' }}>{item.name}</a>
+          </div>
 
-       <Course_cards 
-          price='$110.00'
-          course_name ='Arts & Humanities'
-          skill='History & Philosophy'
-          name='Adam Smith'
-          img={Card3}
-        />
+          <div className="card_info">
+            <h2>{item.course_name}</h2>
+          </div>
 
-        <Course_cards 
-          price='$130.00'
-          course_name ='Language Learning'
-          skill='English learning'
-          name='Tomash Jarek'
-          img={Card4}
-        />
-         
-         <Course_cards 
-          price='$190.00'
-          course_name ='Business'
-          skill='Business Strategy'
-          name='Adam Smith'
-          img={Card5}
-        />
+          <div className="card_icons">
+            <div className="icon">
+              <span>{<TbClockHour4Filled />}</span>
+              <p>10 Weeks</p>
+            </div>
 
-        <Course_cards 
-          price='$100.00'
-          course_name ='Arts & Humanities'
-          skill='Music & Art'
-          name='Adam Smith'
-          img={Card6}
-        />
-        
-        <Course_cards 
-          price='$135.00'
-          course_name ='Data Science'
-          skill='Data Analysis'
-          name='Adam Smith'
-          img={Card7}
-        />
+            <div className="icon">
+              <span>{<FaSignal />}</span>
+              <p>All Levels</p>
+            </div>
 
-        <Course_cards 
-          price='$200.00'
-          course_name ='Math & Logic'
-          skill='Algebra & Calculus'
-          name='Adam Smith'
-          img={Card8}
-        />
+            <div className="icon">
+              <span>{<MdCollectionsBookmark />}</span>
+              <p>Lessons</p>
+            </div>
 
-        <div className="course_pages">
-            <button style={{backgroundColor:'#007bff', color:'#FFFF'}}>1</button>
-            <button>2</button>
-            <button style={{marginBottom:'20px'}}><GrLinkNext/></button>
+            <div className="icon">
+              <span>{<FaPuzzlePiece />}</span>
+              <p>Quiz</p>
+            </div>
+
+            <div className="icon">
+              <span>{<PiStudentBold />}</span>
+              <p>0 Students</p>
+            </div>
+          </div>
+          <div className="card_text">
+            <p>Fusi absolutam contra licentiam contenti verba tu solebat callido
+              anteponebas positum amicos moriare praestantia disputare…</p>
+          </div>
+
+          <div className="card_price">
+            <p>{item.price}</p>
+            <Link to={'/view'}><button>View More</button></Link>
+          </div>
         </div>
 
-       
+      </div>
+))} 
+      <div className="course_pages">
+        <button style={{ backgroundColor: '#007bff', color: '#FFFF' }}>1</button>
+        <button>2</button>
+        <button style={{ marginBottom: '20px' }}><GrLinkNext /></button>
+      </div>
+
+
     </main>
-)
+  )
 }
 
 export default Course;
