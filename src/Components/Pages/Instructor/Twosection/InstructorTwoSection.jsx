5a236feefb3b140baf8e4e2a9322668f1c57d18e
Farhad Sultanov <sutlanovv991@gmail.com>
2024-07-13 17:41:30 +0400

    Instructor, Events added

---
+++ src/Components/Pages/Instructor/Twosection/InstructorTwoSection.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import "./InstructorTwoSection.css";
+import InstructorTwoSectionOneImg from "../../../../../src/assets/Pages/Instructor/avatar1.png";
+import InstructorTwoSectionTwoImg from "../../../../assets/Pages/Instructor/avatar2.png";
+import InstructorTwoSectionThreeImg from "../../../../assets/Pages/Instructor/avatar3.png";
+import InstructorTwoSectionFourImg from "../../../../assets/Pages/Instructor/avatar4.png";
+import InstructorTwoSectionFiveImg from "../../../../assets/Pages/Instructor/avatar5.png";
+import InstructorTwoSectionSixImg from "../../../../assets/Pages/Instructor/avatar6.png";
+import InstructorTwoSectionSevenImg from "../../../../assets/Pages/Instructor/avatar7.png";
+import InstructorTwoSectionEightImg from "../../../../assets/Pages/Instructor/avatar8.png";
+
+const InstructorTwoSection = () => {
+  return (
+    <div className="InstructorOneSectionContainer">
+      <div className="InstructorOneSectionTextPart">
+        <h2>Top Instructor</h2>
+        <p>
+          Jeffrey crikey victoria sponge mush spiffing super arse over tit matie
+          boy smashing. The little rotter off his nut codswallop.!
+        </p>
+      </div>
+      <div className="InstructorOneSectionCardsPart">
+        <div className="InstructorOneSectionCard">
+          <div className="InstructorOneSectionCardUpPart">
+            <img src={InstructorTwoSectionOneImg} alt="Instructor_TwoSection_OneImg" />
+          </div>
+            <div className="InstructorOneSectionCardDownPart">
+              <h4>Dylan Meringue</h4>
+              <p>Sales Director</p>
+            </div>
+        </div>
+        <div className="InstructorOneSectionCard">
+          <div className="InstructorOneSectionCardUpPart">
+            <img src={InstructorTwoSectionOneImg} alt="Instructor_TwoSection_OneImg" />
+          </div>
+            <div className="InstructorOneSectionCardDownPart">
+              <h4>Dylan Meringue</h4>
+              <p>Sales Director</p>
+            </div>
+        </div>
+        <div className="InstructorOneSectionCard">
+          <div className="InstructorOneSectionCardUpPart">
+            <img src={InstructorTwoSectionOneImg} alt="Instructor_TwoSection_OneImg" />
+          </div>
+            <div className="InstructorOneSectionCardDownPart">
+              <h4>Dylan Meringue</h4>
+              <p>Sales Director</p>
+            </div>
+        </div>
+        <div className="InstructorOneSectionCard">
+          <div className="InstructorOneSectionCardUpPart">
+            <img src={InstructorTwoSectionOneImg} alt="Instructor_TwoSection_OneImg" />
+          </div>
+            <div className="InstructorOneSectionCardDownPart">
+              <h4>Dylan Meringue</h4>
+              <p>Sales Director</p>
+            </div>
+        </div>
+        <div className="InstructorOneSectionCard">
+          <div className="InstructorOneSectionCardUpPart">
+            <img src={InstructorTwoSectionOneImg} alt="Instructor_TwoSection_OneImg" />
+          </div>
+            <div className="InstructorOneSectionCardDownPart">
+              <h4>Dylan Meringue</h4>
+              <p>Sales Director</p>
+            </div>
+        </div>
+        <div className="InstructorOneSectionCard">
+          <div className="InstructorOneSectionCardUpPart">
+            <img src={InstructorTwoSectionOneImg} alt="Instructor_TwoSection_OneImg" />
+          </div>
+            <div className="InstructorOneSectionCardDownPart">
+              <h4>Dylan Meringue</h4>
+              <p>Sales Director</p>
+            </div>
+        </div>
+        <div className="InstructorOneSectionCard">
+          <div className="InstructorOneSectionCardUpPart">
+            <img src={InstructorTwoSectionOneImg} alt="Instructor_TwoSection_OneImg" />
+          </div>
+            <div className="InstructorOneSectionCardDownPart">
+              <h4>Dylan Meringue</h4>
+              <p>Sales Director</p>
+            </div>
+        </div>
+        <div className="InstructorOneSectionCard">
+          <div className="InstructorOneSectionCardUpPart">
+            <img src={InstructorTwoSectionOneImg} alt="Instructor_TwoSection_OneImg" />
+          </div>
+            <div className="InstructorOneSectionCardDownPart">
+              <h4>Dylan Meringue</h4>
+              <p>Sales Director</p>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default InstructorTwoSection;
