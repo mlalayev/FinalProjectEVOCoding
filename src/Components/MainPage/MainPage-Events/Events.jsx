ea6f635e5a568b4f662a6a8bc6492b4da42cd1c8
Murad Lalayev <lalayevmurad@gmail.com>
2024-07-13 19:39:13 +0400

    my commit

--- src/Components/MainPage/MainPage-Events/Events.jsx
+++ src/Components/MainPage/MainPage-Events/Events.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import './Events.css';
 import { IoTimeOutline, IoLocationOutline } from "react-icons/io5";
 import eventsData from '../../../../Events.json'; // Adjust the path as necessary
+import { useTranslation } from 'react-i18next';
 
 function Events() {
+    const { t, i18n } = useTranslation();
+
+    // Determine which language data to use based on current i18next language
+    let lang = i18n.language.substr(0, 2); // Get language code (en, az, ru)
+    let events = eventsData[lang] || []; // Default to empty array if language data not found
+
     return (
         <div className='center column'>
-            <h1>Our Upcoming Events</h1>
-            <p>Jeffrey crikey victoria sponge mush spiffing super arse over tit matie boy smashing. The little rotter off his nut codswallop.!</p>
+            <h1>{t('events.title')}</h1>
+            <p>{t('events.intro')}</p>
 
             <div className="events-holder">
-                {eventsData.map((event, index) => (
+                {events.map((event, index) => (
                     <div className="events" key={index}>
                         <div className="image-part-events">
                             <img src={event.image} alt="" className='events-image' />
