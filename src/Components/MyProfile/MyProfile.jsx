97d50ffaa789916fe9e130c8af96e4a2c1e4b40c
Murad Lalayev <lalayevmurad@gmail.com>
2024-07-27 21:23:58 +0400

    mu commit

--- src/Components/MyProfile/MyProfile.jsx
+++ src/Components/MyProfile/MyProfile.jsx
@@ -1,8 +1,8 @@
-// MyProfile.js
+import './MyProfile.css';
 import React from 'react';
+import '../Common/Root.css';
 import Sidebar from './Sidebar/Sidebar.jsx';
 import Dashboard from './DashBoard/DashBoard.jsx';
-import './MyProfile.css'
 
 const MyProfile = () => {
   return (
